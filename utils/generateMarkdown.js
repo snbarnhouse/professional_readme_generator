2b8a5d7e5c0bdfbdb617c7075c54dcdd07c7e25a
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-04-04 13:41:03 -0400

    Added to markdownjs file.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,124 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Generate markdown for README
+function generateMarkdown(userResponses, userInfo) {
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  //userReponses in table of contents
+  let draftTable = `## Table of Contents`;
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  if (userResponses.installation !== '') { draftTable += `
+  * [Installation](#installation)` };
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+  if (userResponses.usage !== '') { draftTable += `
+  * [Usage](#usage)` };
 
-`;
-}
+  if (userResponses.contributing !== '') { draftTable += `
+  * [Contributing](#contributing)` };
 
+  if (userResponses.tests !== '') { draftTable += `
+  * [Tests](#tests)` };
+  
+  // Create title and description
+  // Generate badges for each
+  let draftMarkdown = 
+  `# ${userResponses.title}
+  ![Badge for GitHub](https://img.shields.io/github/languages/top/${userResponses.username}/${userResponses.repository}?style=flat&logo=appveyor) 
+  
+  
+  ## Description 
+  
+  
+  ${userResponses.description}
+  `
+  //Add table of contents
+  draftMarkdown += draftTable;
+  
+  //Add license section
+  draftMarkdown += `
+  * [License](#license)`;
+
+  //Installation section
+  if (userResponses.installation !== '') {
+  
+  draftMarkdown +=
+  `
+  
+  ## Installation
+  
+  
+  ${userResponses.installation}`
+  };
+
+  //Usage section
+  if (userResponses.usage !== '') {
+  
+  draftMarkdown +=
+  
+  `
+  
+  ## Usage 
+  
+   
+  ${userResponses.usage}`
+  };
+  
+  //Contribution section
+  if (userResponses.contributing !== '') {
+  `
+  
+  ## Contributing
+  
+  
+  ${userResponses.contributing}`
+  };
+
+  //Tests section
+  if (userResponses.tests !== '') {
+  
+  draftMarkdown +=
+  `
+  
+  ## Tests
+  
+  
+  ${userResponses.tests}`
+  };
+
+  //License section
+  draftMarkdown +=
+  `
+  
+  ## License
+  
+  ${userResponses.license}
+  `;
+
+  //Questions section
+  let draftDeveloper = 
+  `
+  ---
+  
+  ## Questions?
+  
+  ![Developer Profile Picture](${userInfo.avatar_url}) 
+  
+  For any questions, please contact me with the information below:
+ 
+  GitHub: [@${userInfo.login}](${userInfo.url})
+  `;
+
+  //If GitHub email is not null, add to Developer section
+  if (userInfo.email !== null) {
+  
+  draftDeveloper +=
+  `
+  Email: ${userInfo.email}
+  `};
+
+ //Developer section
+  draftMarkdown += draftDeveloper;
+
+  //Return input
+  return draftMarkdown;
+};
+
+// Export markdown
 module.exports = generateMarkdown;
